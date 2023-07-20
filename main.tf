fdefa983ed0c47a4426844e93d8f0f2d29676a42
Samuel Hwang <shwang@bomberosquito.gob.ec>
2023-07-20 08:41:20 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-05bfc1ab11bfbf484"
+  instance_type = "t2.micro"
+}
+
