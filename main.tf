7ba92dd632e4c93b69ad7a9798ab8b5971920d5c
Samuel Hwang <shwang@bomberosquito.gob.ec>
2023-07-26 11:26:53 -0500

    autoscaling group instead of EC2 instance with launch config and defaults vpc and subnets

--- main.tf
+++ main.tf
@@ -12,21 +12,56 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-05bfc1ab11bfbf484"
+# resource "aws_instance" "example" {
+#   ami = "ami-05bfc1ab11bfbf484"
+#   instance_type = "t2.micro"
+#   vpc_security_group_ids = [aws_security_group.instance.id]
+
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               echo "Hello World" > index.html
+#               nohup busybox httpd -f -p 8080 &
+#               EOF
+  
+#   user_data_replace_on_change = true
+
+#   tags = {
+#     Name = "terraform-ec2-example"
+#   }
+# }
+
+resource "aws_launch_configuration" "config" {
+  image_id = "ami-05bfc1ab11bfbf484"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello World" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
-  
-  user_data_replace_on_change = true
 
-  tags = {
-    Name = "terraform-ec2-example"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "bar" {
+  name = "terraform-autoscaling_group"
+  launch_configuration = aws_launch_configuration.config.name
+  min_size = 1
+  max_size = 3
+  vpc_zone_identifier = data.aws_subnets.default.ids
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
 }
 
@@ -38,7 +73,7 @@ resource "aws_security_group" "instance" {
     to_port = var.server-port
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-
+    description = "terraform-inbound-rule"
   }
 }
 
@@ -48,9 +83,9 @@ variable "server-port" {
   default = 8080
 }
 
-output "public-ip" {
-  value = aws_instance.example.public_ip
-  description = "Public IP of the web server"
-}
+# output "public-ip" {
+#   value = aws_instance.example.public_ip
+#   description = "Public IP of the web server"
+# }
 
 
